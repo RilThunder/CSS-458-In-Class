e8f6b8fd9b7f87bc0c7d29782896a019f0afc590
Thuan Tran <oda1234@uw.edu>
2017-05-20 15:46:36 -0700

    Added Constants for Global and updated Visualize

--- Visualize.py
+++ Visualize.py
@@ -5,7 +5,10 @@ import Player
 
 def main():
     numberOfGame = 0
-    while (numberOfGame < 100):
+
+    while (numberOfGame < Global.NUMBER_OF_SIMULATION):
+        win = 0  # Win/Loss ratio
+        lose = 0
         theDealer = Dealer(None, Global.NUMBER_OF_DECKS)
         listOfPlayer = []
         for i in range(Global.NUMBER_OF_PLAYER):
@@ -13,12 +16,14 @@ def main():
             listOfPlayer.append(player)  # Each player now have this dealer
         # Asigning the list of player back to the dealer
         theDealer.listOfPlayer = listOfPlayer
+        numberRounds = 0
 
-        while (True):  # Play each round until player lose or something
+        while (numberRounds < Global.NUMBER_OF_ROUNDS_):  # Play each round until player lose or something
             theDealer.deal(False, None)
             for i in range(Global.NUMBER_OF_PLAYER):
                 listOfPlayer[i].play()
             theDealer.play()
+            numberRounds += 1
 
         # Finished one whole simulation
         numberOfGame += 1
