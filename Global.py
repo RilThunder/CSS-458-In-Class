cb5b6630e4e320483dd7b9e06cace8b80c835654
saihimakar <himakar_s@yahoo.com>
2017-05-20 21:11:19 -0700

    Updated Global class

--- Global.py
+++ Global.py
@@ -2,7 +2,7 @@ import numpy as np
 NUMBER_OF_PLAYER = 4
 STARTING_CHIPS = 500
 NUMBER_OF_DECKS = 2
-playerSeen = np.array()
+#playerSeen = np.array()
 BUY_IN = 20
 MAX_BET =  100
 NUMBER_OF_SIMULATION = 100
