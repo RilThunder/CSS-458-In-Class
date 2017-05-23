b71be981e3098664c1dd960511e7c27e227f7d6a
TKMsports <macinnest@yahoo.com>
2017-05-23 16:02:41 -0700

    Update Global.py
    change buy_in to 10

--- Global.py
+++ Global.py
@@ -7,7 +7,7 @@ These are the initial value, but can be changed during the simulation.
 NUMBER_OF_PLAYER = 4   # Number of Players participating the simulation
 STARTING_CHIPS = 500   # Starting chips beginning of every simulation per Player
 NUMBER_OF_DECKS = 2    # Number of decks used for the simulation
-BUY_IN = 20            # Minimum Bet to play the blackjack
+BUY_IN = 10            # Minimum Bet to play the blackjack
 MAX_BET =  100         # The maximum bet per 1 round of game
 NUMBER_OF_SIMULATION = 30       # Number of Simulation to be done
 NUMBER_OF_ROUNDS = 20           # Number of rounds per simulation
