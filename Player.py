f612635a82b93e7f03ff8ee3cae8b5bcabd099f7
saihimakar <himakar_s@yahoo.com>
2017-05-23 15:15:12 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -15,7 +15,7 @@ class Player:
         self.numOfChips = Global.STARTING_CHIPS
         self.dealer = Dealer
         self.confidenceLevel = .5
-        self.secondHandCard = []
+        self.secondHandCard = []          # secondhand used for the split()
         self.choice = 3
         self.split = False
         self.didDouble = False
@@ -34,7 +34,9 @@ class Player:
     When the player call this, it will act like a double in the casino.
     Have to decide probability. 
     """""
+    
     def double(self):
+        # In our simulation we are going to double between 9 to 11 inclusive.
         self.currentBet = Global.BUY_IN * 2
         self.didDouble = True
         self.stand = True
@@ -45,6 +47,8 @@ class Player:
     When the player call this, it will act like an actual split in the casino 
     """""
     def split(self):
+        # Checks to see if the length of the card is 2 and if the index value 
+        # of the card at 0 is equalt to the index value of card at 1. 
         if len(self.firstHandCard) == 2 and self.firstHandCard[0] == self.firstHandCard[1]:
             self.secondHandCard.append(self.firstHandCard[1])
             self.firstHandCard.remove(self.firstHandCard[1])
@@ -84,6 +88,11 @@ class Player:
                 self.stand = True
                 # self.dealer.deal(True, self)
 
+   
+    
+    
+  # This play functions allow the player to choose if they want to play by the
+ # odds, random or Normal.  
     def play(self):
         if self.choice == 1:
             self.playWithOdds()
@@ -111,7 +120,7 @@ class Player:
         # Need to check if busted as well. If busted then 21 - totalValue will be negative
 
 
-            # Calculating the remainng cards to determine the probabiltity
+            # Calculating the remaning cards to determine the probabiltity
 
             totalValue = np.sum(np.asarray(self.firstHandCard))
             remainingWeNeed = 21 - totalValue
@@ -122,6 +131,8 @@ class Player:
                     count+=1
             probWeShouldContinue = count/lengthOfTotal
             choice = random.random()
+            # if the probability is greater than or eqault to the choice then
+            # the player continues to hit or if not then they will stand. 
             if probWeShouldContinue >= choice:
 
                 self.hit()
