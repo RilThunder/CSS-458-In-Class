c81f0d666d81910d97fbc4747c32ada1d30b98d5
dori88 <gomdori88@gmail.com>
2017-05-23 13:25:48 -0700

    Update Global.py

--- Global.py
+++ Global.py
@@ -1,9 +1,15 @@
-NUMBER_OF_PLAYER = 4
-STARTING_CHIPS = 500
-NUMBER_OF_DECKS = 2
+"""""
+Global Variable to be used in Blackjack Simulation.
+Variables are first declared in here, and is used in various classes.
+These are the initial value, but can be changed during the simulation.
+"""""
+
+NUMBER_OF_PLAYER = 4   # Number of Players participating the simulation
+STARTING_CHIPS = 500   # Starting chips beginning of every simulation per Player
+NUMBER_OF_DECKS = 2    # Number of decks used for the simulation
+BUY_IN = 20            # Minimum Bet to play the blackjack
+MAX_BET =  100         # The maximum bet per 1 round of game
+NUMBER_OF_SIMULATION = 30       # Number of Simulation to be done
+NUMBER_OF_ROUNDS = 20           # Number of rounds per simulation
+DEALER_SOFT_SEVENTEEN = False   # Assign soft17 rule for the Dealer
 #playerSeen = np.array()
-BUY_IN = 20
-MAX_BET =  100
-NUMBER_OF_SIMULATION = 30
-NUMBER_OF_ROUNDS = 20
-DEALER_SOFT_SEVENTEEN = False
