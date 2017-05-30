6895d948acec6c0d76d3c3b64e33bc8306c7be68
dori88 <gomdori88@gmail.com>
2017-05-29 22:48:16 -0700

    Update Visualize.py

--- Visualize.py
+++ Visualize.py
@@ -169,7 +169,7 @@ def main():
             # Initialize the Player(s) for this simulation
             listOfPlayer = []
             for i in range(Global.NUMBER_OF_PLAYER): # Player number is decided on NUMBER_OF_PLAYER
-                player = Player(theDealer, Global.WAY_TO_PLAY)
+                player = Player(theDealer, Global.WAY_TO_PLAY, Global.BASE_CONFIDENT)
                 listOfPlayer.append(player)  # Each player now have this dealer
                 
             # Asigning the list of player back to the dealer
